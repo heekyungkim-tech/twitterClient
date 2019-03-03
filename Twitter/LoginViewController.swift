7365e269d11124558559bd140b267da10bfc8fa3
Heekyung Kim <Heekyungkim.tech@gmail.com>
2019-03-02 19:17:21 -0500

    twitter part1

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Heekyung kim on 3/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.cornerRadius = 15
+    }
+    
+
+    @IBAction func onTopLogin(_ sender: Any) {
+        let urlString = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url:urlString, success: {
+            
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        }, failure: {(Error_) in
+        
+        print("Error.Could not log in.")
+            
+            let title = "Error"
+            let message = "An error has occured. Unable to log-in."
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+    })
+    }
+}
